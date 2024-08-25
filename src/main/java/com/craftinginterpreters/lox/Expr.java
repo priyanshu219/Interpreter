88a752886afcf5c7019ff1544198d711cbf17825
Priyanshu Garg <priyanshu.g.garg@oracle.com>
2024-08-26 00:15:56 +0530

    Made subclass as public

--- src/main/java/com/craftinginterpreters/lox/Expr.java
+++ src/main/java/com/craftinginterpreters/lox/Expr.java
@@ -11,7 +11,7 @@ abstract class Expr {
         R visitUnaryExpr(Unary expr);
     }
 
-    static class Binary extends Expr {
+    public static class Binary extends Expr {
         Binary(Expr left, Token operator, Expr right) {
             this.left = left;
             this.operator = operator;
@@ -29,7 +29,7 @@ abstract class Expr {
         final Expr right;
     }
 
-    static class Grouping extends Expr {
+    public static class Grouping extends Expr {
         Grouping(Expr expression) {
             this.expression = expression;
         }
@@ -43,7 +43,7 @@ abstract class Expr {
         final Expr expression;
     }
 
-    static class Literal extends Expr {
+    public static class Literal extends Expr {
         Literal(Object value) {
             this.value = value;
         }
@@ -57,7 +57,7 @@ abstract class Expr {
         final Object value;
     }
 
-    static class Unary extends Expr {
+    public static class Unary extends Expr {
         Unary(Token operator, Expr right) {
             this.operator = operator;
             this.right = right;
